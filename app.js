53b88968dd036cbc090ff45a8ef0e8e05dd6b72b
Su Oo Lwin <suoolwin88@gmail.com>
2024-08-02 01:51:06 +0630

    Aug2 1:50am

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //Get UI
+ 
+
+// Start Back to Top 
+
+    $(".btn-backtotops").hide();
+    $(window).scroll(function(){
+
+        let getscrolltop = $(this).scrollTop();
+            // console.log(getscrolltop);
+
+            if(getscrolltop >= 360){
+                $(".btn-backtotops").fadeIn(1000);
+            }else{
+                $(".btn-backtotops").fadeOut(1000);
+            }
+    });
+
+
+// End Back to Top 
+
+// Start Footer Section 
+
+    const getyear = document.getElementById("getyear");
+    const getfullyear = new Date().getFullYear();
+    getyear.textContent = getfullyear;
+
+
+// End Footer Section 
